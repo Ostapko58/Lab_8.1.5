b79bf1292dffbf5868a4bb6475c3937f3b1078f7
Ostap Onyshkiv <prostoostap58@gmail.com>
2022-03-30 20:18:11 +0300

    adeed files

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_8.1.5/Lab_8.1.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[] = ",----sdfsdfsd - ,-,-,-,sdfsdfs";
+			Assert::AreEqual(Count(str), 4);
+		}
+	};
+}
